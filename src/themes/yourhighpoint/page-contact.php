0269c21c856ffd3fff946dfb19f71a8c7ca199d9
charllab <developer.charl@gmail.com>
2019-11-08 10:38:08 -0800

    added FAX to contact page template

--- src/themes/yourhighpoint/page-contact.php
+++ src/themes/yourhighpoint/page-contact.php
@@ -28,7 +28,6 @@ get_header(); ?>
             </div>
         </div>
 
-
         <div class="section--ping-pong p-0 alignfull">
             <div class="container-fluid no-gutters p-0">
                 <div class="row no-gutters mb-1">
@@ -40,36 +39,64 @@ get_header(); ?>
                     >
                     </div>
                     <div class="col-xl-5 js--ping-pong__content-block">
-                        <div class="section--ping-pong__content-page h-100 d-flex flex-column justify-content-center px-1 py-xl-2 px-lg-3">
+                        <div
+                            class="section--ping-pong__content-page h-100 d-flex flex-column justify-content-center px-1 py-xl-2 px-lg-3">
 
-
-                            <div class="content-detail__block pt-2 pb-1 pt-sm-3 pb-sm-2 py-lg-2 px-sm-4 px-lg-4 px-xxl-6 text-center text-xl-left">
+                            <div
+                                class="content-detail__block pt-2 pb-1 pt-sm-3 pb-sm-2 py-lg-2 px-sm-4 px-lg-4 px-xxl-6 text-center text-xl-left">
                                 <h2 class="h1">Our Clinic</h2>
                                 <?php
                                 $removethese = array("(", " ", ")", "-");
                                 ?>
                                 <table class="w-100">
                                     <tr>
-                                        <td><span class="contact-details text-uppercase">Phone: <a
+                                        <td>
+                                            <span class="contact-details text-uppercase font-weight-bold">Phone: </span>
+                                            <span class="contact-details text-uppercase">
+                                                <a
                                                     href="tel:+1<?php echo strip_tel(get_field('phone_number', 'option')); ?>"
-                                                    class="text-body"><?php echo get_field('phone_number', 'option'); ?></a></span>
+                                                    class="text-body"><?php echo get_field('phone_number', 'option'); ?>
+                                                </a>
+                                            </span>
                                         </td>
                                     </tr>
                                     <tr>
                                         <td>&nbsp;</td>
                                     </tr>
                                     <tr>
-                                        <td><span class="contact-details text-uppercase">E-mail: <a
-                                                    href="mailto:<?php echo get_field('email_address', 'option'); ?>"
-                                                    class="text-body"><?php echo get_field('email_address', 'option'); ?></a></span>
+                                        <td><span class="contact-details text-uppercase font-weight-bold">Fax: </span>
+                                            <span class="contact-details text-uppercase">
+                                                <a
+                                                    href="tel:+1<?php echo strip_tel(get_field('fax_number', 'option')); ?>"
+                                                    class="text-body"><?php echo get_field('fax_number', 'option'); ?>
+                                                </a>
+                                            </span>
                                         </td>
                                     </tr>
                                     <tr>
                                         <td>&nbsp;</td>
                                     </tr>
-                                    <tr style="vertical-align: top;">
-                                        <td><span
-                                                class="contact-details text-uppercase">Address: <?php echo get_field('physical_address', 'option'); ?><span></span>
+                                    <tr>
+                                        <td>
+                                            <span
+                                                class="contact-details text-uppercase font-weight-bold">E-mail: </span>
+                                            <span class="contact-details text-uppercase">
+                                                    <a
+                                                        href="mailto:<?php echo get_field('email_address', 'option'); ?>"
+                                                        class="text-body"><?php echo get_field('email_address', 'option'); ?>
+                                                    </a>
+                                            </span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <span
+                                                class="contact-details text-uppercase font-weight-bold">Address: </span>
+                                            <span
+                                                class="contact-details text-uppercase"><?php echo get_field('physical_address', 'option'); ?></span>
                                         </td>
                                     </tr>
                                 </table>
@@ -98,7 +125,8 @@ get_header(); ?>
 
         <?php if (get_field('map_image', 'option')) {
             ; ?>
-            <div class="container px-0 contact-page__map-block alignfull bg-size-cover position-relative" style="background-image: url(<?php echo get_field('map_image', 'option'); ?>)">
+            <div class="container px-0 contact-page__map-block alignfull bg-size-cover position-relative"
+                 style="background-image: url(<?php echo get_field('map_image', 'option'); ?>)">
                 <a href="<?php echo get_field('map_link', 'option'); ?>"
                    class="contact-page__map-link postion-absolute w-100 h-100 d-block"
                    target="_blank"
