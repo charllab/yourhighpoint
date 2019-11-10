66d966b83cec5373f346bf99d8e1a907c148ddc1
charllab <developer.charl@gmail.com>
2019-11-09 17:06:31 -0800

    adding of legalities

--- src/themes/yourhighpoint/footer.php
+++ src/themes/yourhighpoint/footer.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-6 text-center text-lg-left">
-                    <p class="mb-0">&copy; <?php echo Date('Y') . ' ' . get_bloginfo('name'); ?></p>
+                    <p class="mb-0">&copy; <?php echo Date('Y') . ' ' . get_bloginfo('name'); ?> | <a href="<?php echo esc_url(home_url('/')); ?>legalities" class="text-offwhite">Legalities</a></p>
                 </div>
                 <div class="col-lg-6 text-center text-lg-right">
                     <p class="mb-0">Designed, Developed and Hosted by <a href="https://sproing.ca" target="_blank" class="text-offwhite">Sproing&nbsp;Creative</a>
