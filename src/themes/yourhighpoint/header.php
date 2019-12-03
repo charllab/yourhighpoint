e38b41022a3aeaca7dbb4badceb3a53e2c431c32
charllab <developer.charl@gmail.com>
2019-12-02 16:05:36 -0800

    added FB Pixel

--- src/themes/yourhighpoint/header.php
+++ src/themes/yourhighpoint/header.php
@@ -28,6 +28,23 @@
         gtag('config', 'UA-2271929-72');
     </script>
 
+    <!-- Facebook Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '438670550400419');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+                   src="https://www.facebook.com/tr?id=438670550400419&ev=PageView&noscript=1"
+        /></noscript>
+    <!-- End Facebook Pixel Code -->
 
     <!-- Hotjar Tracking Code for https://yourhighpoint.ca -->
     <script>
