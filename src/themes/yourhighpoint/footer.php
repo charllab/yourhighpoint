8bdf90af6e1bc623ea0921e27562dbd4a18ffff9
charllab <developer.charl@gmail.com>
2023-06-14 12:42:00 -0700

    STRUCTURE: added checkpoints for new server

--- src/themes/yourhighpoint/footer.php
+++ src/themes/yourhighpoint/footer.php
@@ -1,4 +1,4 @@
-<footer class="bg-footer">
+<footer class="bg-footer runcloud">
     <section class="section text-md text-offwhite py-1">
         <div class="container">
             <div class="row">
@@ -6,7 +6,7 @@
                     <p class="mb-1 mb-sm-50 mb-lg-0">&copy; <?php echo Date('Y') . ' ' . get_bloginfo('name'); ?> |&nbsp;<a href="<?php echo esc_url(home_url('/')); ?>legalities" class="text-offwhite">Legalities</a></p>
                 </div>
                 <div class="col-lg-6 text-center text-lg-right">
-                    <p class="mb-0">Designed, Developed and Hosted by <a href="https://sproing.ca" target="_blank" class="text-offwhite">Sproing&nbsp;Creative</a>
+                    <p class="mb-0">Designed, Developed and Hosted by <a href="https://sproing.ca" target="_blank" class="text-offwhite">Sproing&nbsp;Creative.</a>
                     </p>
                 </div>
             </div>
